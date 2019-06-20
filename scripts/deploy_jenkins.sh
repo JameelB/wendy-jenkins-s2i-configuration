35f830bb18e14ad581d50320eb632b0a36155af3
Jameel Briones <jbriones@redhat.com>
2019-06-20 10:22:47 +0100

    remove kubernetes slave setup

--- scripts/deploy_jenkins.sh
+++ scripts/deploy_jenkins.sh
@@ -1,28 +1,8 @@
 #!/bin/sh -xe
-TEMPLATES_DIR="/home/jbriones/go/src/github.com/feedhenry/wendy-jenkins-s2i-configuration/templates"
-DOCKER_USERNAME=""
-DOCKER_PASSWORD=""
-DOCKER_EMAIL=""
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TEMPLATES_DIR="$( cd $SCRIPTS_DIR/../templates && pwd )"
 
 oc new-project $PROJECT_NAME
-
-oc secrets new-dockercfg dockerhub --docker-server=docker.io --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL
-
-for AGENT in java-ubuntu jenkins-tools nodejs-ubuntu nodejs6-ubuntu ruby ruby-fhcap ansible go-centos7 python2-centos7 nodejs6-centos7 svcat circleci
-do
-    AGENT_LABELS="$AGENT ${AGENT/-/ } openshift"
-
-    if [ "$AGENT" = "nodejs-ubuntu" ] ; then
-        AGENT_LABELS="ubuntu nodejs4-ubuntu"
-    fi
-
-    if [ "$AGENT" = "nodejs6-ubuntu" ] ; then
-        AGENT_LABELS="ubuntu nodejs6-ubuntu"
-    fi
-
-    oc new-app -p AGENT_LABEL="$AGENT_LABELS" -p IMAGE_NAME=jenkins-agent-$AGENT -f  $TEMPLATES_DIR/agent-image-template.yml
-done
-
 oc new-app -f  $TEMPLATES_DIR/jenkins-image-template.yaml
 
 # Adding create project access to system:serviceaccount:$PROJECT_NAME:jenkins
