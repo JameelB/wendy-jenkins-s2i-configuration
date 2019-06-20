0dea6ebd8adaeef8308c744ad40d52456ba6492b
Jameel Briones <jbriones@redhat.com>
2019-06-20 09:36:28 +0100

    add import image as last step

--- scripts/deploy_jenkins.sh
+++ scripts/deploy_jenkins.sh
@@ -29,3 +29,4 @@ oc new-app -f  $TEMPLATES_DIR/jenkins-image-template.yaml
 # Adding create project access to system:serviceaccount:$PROJECT_NAME:jenkins
 oc adm policy add-cluster-role-to-user self-provisioner system:serviceaccount:$PROJECT_NAME:jenkins
 oc new-app -p MEMORY_LIMIT=2Gi -p NAMESPACE=$PROJECT_NAME -p JENKINS_IMAGE_STREAM_TAG=jenkins:latest -f  $TEMPLATES_DIR/jenkins-template.yml
+oc import-image jenkins:latest -n $PROJECT_NAME
